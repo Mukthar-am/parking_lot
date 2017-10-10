8749a3ed3f9084506d643a90294b2615b476530e
mukthar-am <mukthar.am@gmail.com>
2017-10-10 21:24:16 +0530

    application package and object structures

---
+++ src/main/java/org/muks/parking/entities/Vehicle.java
@@ -0,0 +1,7 @@
+package org.muks.parking.entities;
+
+/**
+ * Created by 300000511 on 10/10/17.
+ */
+public class Vehicle {
+}
---
+++ src/main/java/org/muks/parking/space/ParkingLot.java
@@ -0,0 +1,7 @@
+package org.muks.parking.space;
+
+/**
+ * Created by 300000511 on 10/10/17.
+ */
+public class ParkingLot {
+}
