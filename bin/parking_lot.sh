db05348838a6f8e428068611331809a9b8882362
mukthar-am <mukthar.am@gmail.com>
2017-10-13 09:44:50 +0530

    a left over bug w.r.t lib path being hard coded in the bin file

--- bin/parking_lot.sh
+++ bin/parking_lot.sh
@@ -3,18 +3,22 @@
 # Start script for parking lot application #
 #echo "# Starting up parking lot application now..."
 
+cd ..
+pwd=`pwd`
+libDir="$pwd/lib"
+
 # Check for the user interaction with the applicatin.
 if [ $# -eq 0 ]
 then
     echo "No arguments supplied"
-    appStartCmd="java -cp /opt/muks/parking_lot/lib/parking_lot-1.0.jar org.muks.parking.manager.AppManager"
+    appStartCmd="java -cp $libDir/parking_lot-1.0.jar org.muks.parking.manager.AppManager"
     $appStartCmd
 
 else
     fileInput=$1    # as first argument is the input file-name, expected to be found at the same path
     #echo "# Parsing input file $fileInput"
 
-    appStartCmd="java -cp /opt/muks/parking_lot/lib/parking_lot-1.0.jar org.muks.parking.manager.AppManager $fileInput"
+    appStartCmd="java -cp $libDir/parking_lot-1.0.jar org.muks.parking.manager.AppManager $fileInput"
     $appStartCmd
 
 fi
